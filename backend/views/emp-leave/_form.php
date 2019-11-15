6347d309b83e823acb2df9e903d0804f7cd8d203
naumanshahid007 <hwhashmi1625@gmail.com>
2019-11-15 14:12:54 +0500

    emp leave form modify

--- backend/views/emp-leave/_form.php
+++ backend/views/emp-leave/_form.php
@@ -1,7 +1,9 @@
 <?php
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-use kartik\date\DatePicker
+use kartik\date\DatePicker;
+use yii\helpers\ArrayHelper;
+use common\models\Employee;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\EmpLeave */
