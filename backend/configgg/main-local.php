86187a774c661a38c1d6875b23c23fa4f6494e3d
Arslan Nasir <arsalan@dexdevs.com>
2019-12-08 16:17:29 +0500

    update stock modified

---
+++ backend/configgg/main-local.php
@@ -0,0 +1,44 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'XjwHDXKq3c0XOfNKzOflgi7MzfQ_bmG5',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.gmail.com',
+                'username' => 'anasshafqat02@gmail.com',
+                'password' => 'anas12345',
+                'port' => '587',
+                'encryption' => 'tls',
+                ],
+            'viewPath' => '@common/mail',
+        ],
+        'view' => [
+            'theme' => [
+                'pathMap' => [
+                    '@app/views' => '@vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-backend-app'
+                ],
+            ],
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    // $config['bootstrap'][] = 'debug';
+    // $config['modules']['debug'] = [
+    //     'class' => 'yii\debug\Module',
+    // ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
