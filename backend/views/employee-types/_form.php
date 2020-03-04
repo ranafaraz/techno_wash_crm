5f0bc3e3ab5b752374e6741146e70464e9ef9592
naumanshahid007 <hwhashmi1625@gmail.com>
2020-03-04 13:24:43 +0500

    fields are capitalized in employee-types form

--- backend/views/employee-types/_form.php
+++ backend/views/employee-types/_form.php
@@ -18,7 +18,7 @@ use kartik\time\TimePicker;
 
     <div class="row">
         <div class="col-md-6">
-            <?= $form->field($model, 'emp_type_name')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'emp_type_name')->textInput(['maxlength' => true,'id' => 'emp_type_name']) ?>
         </div>
         <div class="col-md-6">
             <?= $form->field($model, 'working_hours')->textInput(['maxlength' => true]) ?>
@@ -37,7 +37,7 @@ use kartik\time\TimePicker;
             <?= $form->field($model, 'monthly_salary')->textInput(['maxlength' => true]) ?>
         </div>
         <div class="col-md-6">
-            <?= $form->field($model, 'description')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'description')->textInput(['maxlength' => true,'id' => 'description']) ?>
         </div>
     </div>
 
@@ -50,6 +50,36 @@ use kartik\time\TimePicker;
     <?php ActiveForm::end(); ?>
     
 </div>
+<script type="text/javascript">
+     function testInput(event) {
+        var value = String.fromCharCode(event.which);
+        var pattern = new RegExp(/[a-zåäö ]/i);
+        return pattern.test(value);
+    }
+    $('#emp_type_name').bind('keypress', testInput);
+    $("#emp_type_name").bind('keyup', function (e) {
+        $("#emp_type_name").val(($("#emp_type_name").val()).toUpperCase());
+    });
+    $('#description').bind('keypress', testInput);
+    $("#description").bind('keyup', function (e) {
+        $("#description").val(($("#description").val()).toUpperCase());
+    });
+  //   function forceKeyPressUppercase(e) {
+  //       var charInput = e.keyCode;
+  //       if((charInput >= 97) && (charInput <= 122)) { // lowercase
+  //         if(!e.ctrlKey && !e.metaKey && !e.altKey) { // no modifier key
+  //           var newChar = charInput - 32;
+  //           var start = e.target.selectionStart;
+  //           var end = e.target.selectionEnd;
+  //           e.target.value = e.target.value.substring(0, start) + String.fromCharCode(newChar) + e.target.value.substring(end);
+  //           e.target.setSelectionRange(start+1, start+1);
+  //           e.preventDefault();
+  //         }
+  //       }
+  // }
+  //       document.getElementById("category_name").addEventListener("keypress", forceKeyPressUppercase, false);
+  //       document.getElementById("category_description").addEventListener("keypress", forceKeyPressUppercase, false);
+</script>
 <?php
 $script = <<< JS
 $(document).ready(function(){
